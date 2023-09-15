43d58ab0155305933ad2e7aa82f371268fe8149f
scyllacore <yms8795@gmail.com>
2023-09-15 21:08:11 +0900

    chore : 프로젝트 생성본 업데이트.

---
+++ src/main/java/com/scyllacore/EasyScrap/EasyScrapApplication.java
@@ -0,0 +1,13 @@
+package com.scyllacore.EasyScrap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyScrapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyScrapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/scyllacore/EasyScrap/EasyScrapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scyllacore.EasyScrap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyScrapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
